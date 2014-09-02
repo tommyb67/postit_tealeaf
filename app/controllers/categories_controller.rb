399567bb9dcfcee662b3db2a125010830508d3e9
Tom Brennan <tommybrennan@gmail.com>
2014-09-02 01:04:40 -0400

    catchup commit getting back to this app

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,32 @@
 class CategoriesController < ApplicationController
 
  def index
-  @categories = Category.all
  end
 
  def new
+  @category = Category.new
  end
 
+
+ def create
+    @category = Category.new(post_params)
+    @category.creator = User.first # TO DO: change once we have authentication
+
+    if @category.save
+      flash[:notice] = "Your category was created"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
  def show
+ end
+
+ private
+
+ def post_params
+  params.require(:category).permit!
  end
 
 end
